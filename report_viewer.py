a22d3a7db2794c2693c8e58d7fd26c09d688d947
Josh Rakovitsky <josh.rakovitsky@outlook.com>
2022-07-15 22:33:02 -0400

    reset repo to prepare for raw file parsing

--- report_viewer.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-from pivottablejs import pivot_ui
-
-master = pd.read_csv("cache\\master.csv", header="infer")
-pivot_ui(master, rows = ["tag", "uom", "name", "report_year", "adsh", "value"], height = "700px", aggregatorName = "Sum")
